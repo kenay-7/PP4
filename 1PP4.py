5ec6b82244355b44e6f8ec3cd935f2c4ffde09aa
kenay-7 <matheus.kenay@sempreceub.com>
2026-05-17 21:37:26 -0300

    att

--- 1PP4.py
+++
@@ -1,6 +0,0 @@
-ct = 0
-VF = int(input('Qual o valor final desejado?'))
-for i in range(0,VF):
-    print(i)
-    i = i + 1
-print(f"Quantidade dos valores: {i}")
--- 2PP4.py
+++
@@ -1,6 +0,0 @@
-ct = 0
-VI = int(input('Qual o valor inicial desejado?'))
-for i in range(VI,-1,-1):
-    print(i)
-    ct = ct + 1
-print(f"Quantidade dos valores: {ct}")
